515e7dd392b5382cfb8541b36a13bcb0bbfb4a67
shilp-ds <shilpi.sharma@dotsquares.com>
2023-03-01 15:57:57 +0530

    ch

--- src/templates/State.tsx
+++ src/templates/State.tsx
@@ -248,11 +248,11 @@ const region: Template<TemplateRenderProps> = ({
 
       if (entity.dm_directoryChildrenCount == 1) {
         entity.dm_directoryChildren.map((res: any) => {
-         //console.log(res,"res")
+         console.log(res,"res")
           let detlslug1 = "";
 
           if (!res.slug) {
-            let slugString = res.id+"-"+res.name();
+            let slugString = res.id + "-" + res.name.toLowerCase();
             let slug = slugString;
             detlslug1 = `${slug}.html`;
           } else {
@@ -263,7 +263,7 @@ const region: Template<TemplateRenderProps> = ({
 
         })
       } else {
-        detlslug = "gb/"+slug+"/"+entity.slug+".html";
+        detlslug = "gb/" + slug + "/" + entity.slug + ".html";
       }
 
     }
